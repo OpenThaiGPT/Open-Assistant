e34021829dcab20308c7fb2b8031d64f3703cf4d
Kobkrit Viriyayudhakorn <kobkrit@gmail.com>
2023-02-28 05:51:58 +0000

    Init verison

--- website/cypress.config.js
+++ website/cypress.config.js
@@ -3,7 +3,7 @@ import getCompareSnapshotsPlugin from "cypress-image-diff-js/dist/plugin";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000",
+    baseUrl: "https://tag.openthaigpt.aieat.or.th",
     setupNodeEvents(on, config) {
       // implement node event listeners here
       getCompareSnapshotsPlugin(on, config);
